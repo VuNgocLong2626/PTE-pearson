3181609eb782fdc39c22ad659ded7365a4050de1
NgocLong <long.speed00@gmail.com>
2022-07-05 15:12:51 +0700

    add domain question

---
+++ app/models/domain/question.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+
+
+class Title(BaseModel):
+    title: str = Field(..., alias="title")
+
+
+class Content(BaseModel):
+    content: int = Field(..., alias='content')
+
+
+class Scores(BaseModel):
+    scores: int = Field(..., alias='scores')
+
+
+class Answer(BaseModel):
+    answer: dict = Field(..., alias='answer')
+
+
+class Solution(BaseModel):
+    solution: dict = Field(...,)
