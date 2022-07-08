fb8a4b1fcef65400fe96d195d724a94f89e58265
NgocLong <long.speed00@gmail.com>
2022-07-08 10:12:10 +0700

    Hotfix get info user

--- app/db/user/get_info_user.py
+++ app/db/user/get_info_user.py
@@ -1,12 +1,11 @@
 from app.utils.aws.dynamodb import table
-from app.models.schemas import user as _user_domain
 from boto3.dynamodb.conditions import Key
 
 
 def get_info_user(
     pk: str,
     sk: str
-) -> _user_domain.UserCreateInDB:
+) -> dict:
     info_user = table.query(
         KeyConditionExpression=Key("PK").eq(pk) &
         Key("SK").eq(sk),
